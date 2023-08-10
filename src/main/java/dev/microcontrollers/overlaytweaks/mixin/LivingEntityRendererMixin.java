03e9000a085a3e257266e893b5b34af053fc820c
MicrocontrollersDev <microcontrollersyt@gmail.com>
2023-08-10 14:54:07 -0700

    add wyvest and mixces contribs

--- src/main/java/dev/microcontrollers/overlaytweaks/mixin/LivingEntityRendererMixin.java
+++ src/main/java/dev/microcontrollers/overlaytweaks/mixin/LivingEntityRendererMixin.java
@@ -10,12 +10,12 @@ import net.minecraft.entity.passive.AbstractHorseEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
+// import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+// import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(LivingEntityRenderer.class)
-public abstract class LivingEntityRendererMixin<T extends LivingEntity> {
+public class LivingEntityRendererMixin<T extends LivingEntity> {
     // TODO: FIGURE OUT WHY THIS DOESNT WORK
 //    @ModifyArgs(method = "render(Lnet/minecraft/entity/LivingEntity;FFLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/EntityModel;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;IIFFFF)V"))
 //    private void transparentHorse(Args args, T livingEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i) {
