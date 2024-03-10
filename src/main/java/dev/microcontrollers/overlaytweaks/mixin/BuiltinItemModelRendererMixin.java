3ee7ab7913c61992502185109a0b60a1a5ebecca
MicrocontrollersDev <microcontrollersyt@gmail.com>
2024-03-10 16:06:13 -0700

    smys fix

--- src/main/java/dev/microcontrollers/overlaytweaks/mixin/BuiltinItemModelRendererMixin.java
+++ src/main/java/dev/microcontrollers/overlaytweaks/mixin/BuiltinItemModelRendererMixin.java
@@ -29,7 +29,7 @@ import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 public class BuiltinItemModelRendererMixin {
     @WrapOperation(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/entity/model/ShieldEntityModel;getLayer(Lnet/minecraft/util/Identifier;)Lnet/minecraft/client/render/RenderLayer;", ordinal = 0))
     private RenderLayer enableShieldTransparency(ShieldEntityModel model, Identifier texture, Operation<RenderLayer> original) {
-        if (OverlayTweaksConfig.CONFIG.instance().customShieldOpacity != 100) return RenderLayer.getEntityTranslucent(texture); // if 0, let's skip the consequences of translucency
+        if (OverlayTweaksConfig.CONFIG.instance().customShieldOpacity != 100) return RenderLayer.getEntityTranslucent(texture); // if 100, let's skip the consequences of translucency
         return original.call (model, texture);
     }
 
@@ -42,27 +42,28 @@ public class BuiltinItemModelRendererMixin {
     // We need to get the parent method parameters for mode, so we need to use @ModifyArgs.
     @ModifyArgs(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/model/ModelPart;render(Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumer;IIFFFF)V"))
     private void changeShieldTransparency(Args args, ItemStack stack, ModelTransformationMode mode, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay) {
-        ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        assert player != null;
-        float cooldown = player.getItemCooldownManager().getCooldownProgress(Items.SHIELD, 0);
-        // We want this to only change shields that are being held so it doesn't affect containers/dropped items, as well as first person only.
-        if (mode == ModelTransformationMode.FIRST_PERSON_LEFT_HAND || mode == ModelTransformationMode.FIRST_PERSON_RIGHT_HAND
-                && OverlayTweaksConfig.CONFIG.instance().customShieldOpacity != 0) {
-            args.set(7, OverlayTweaksConfig.CONFIG.instance().customShieldOpacity / 100);
-            // shield colors
-            if (!OverlayTweaksConfig.CONFIG.instance().colorShieldCooldown) return;
-            if (cooldown <= 1.0F && cooldown > 0.65F) {
-                args.set(4, OverlayTweaksConfig.CONFIG.instance().shieldColorHigh.getRed() / 255F);
-                args.set(5, OverlayTweaksConfig.CONFIG.instance().shieldColorHigh.getGreen() / 255F);
-                args.set(6, OverlayTweaksConfig.CONFIG.instance().shieldColorHigh.getBlue() / 255F);
-            } else if (cooldown <= 0.65F && cooldown > 0.35F) {
-                args.set(4, OverlayTweaksConfig.CONFIG.instance().shieldColorMid.getRed() / 255F);
-                args.set(5, OverlayTweaksConfig.CONFIG.instance().shieldColorMid.getGreen() / 255F);
-                args.set(6, OverlayTweaksConfig.CONFIG.instance().shieldColorMid.getBlue() / 255F);
-            } else if (cooldown <= 0.35F && cooldown > 0F) {
-                args.set(4, OverlayTweaksConfig.CONFIG.instance().shieldColorLow.getRed() / 255F);
-                args.set(5, OverlayTweaksConfig.CONFIG.instance().shieldColorLow.getGreen() / 255F);
-                args.set(6, OverlayTweaksConfig.CONFIG.instance().shieldColorLow.getBlue() / 255F);
+        if (MinecraftClient.getInstance().player != null) {
+            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+            float cooldown = player.getItemCooldownManager().getCooldownProgress(Items.SHIELD, 0);
+            // We want this to only change shields that are being held so it doesn't affect containers/dropped items, as well as first person only.
+            if (mode == ModelTransformationMode.FIRST_PERSON_LEFT_HAND || mode == ModelTransformationMode.FIRST_PERSON_RIGHT_HAND
+                    && OverlayTweaksConfig.CONFIG.instance().customShieldOpacity != 0 && OverlayTweaksConfig.CONFIG.instance().customShieldOpacity != 100) {
+                args.set(7, OverlayTweaksConfig.CONFIG.instance().customShieldOpacity / 100);
+                // shield colors
+                if (!OverlayTweaksConfig.CONFIG.instance().colorShieldCooldown) return;
+                if (cooldown <= 1.0F && cooldown > 0.65F) {
+                    args.set(4, OverlayTweaksConfig.CONFIG.instance().shieldColorHigh.getRed() / 255F);
+                    args.set(5, OverlayTweaksConfig.CONFIG.instance().shieldColorHigh.getGreen() / 255F);
+                    args.set(6, OverlayTweaksConfig.CONFIG.instance().shieldColorHigh.getBlue() / 255F);
+                } else if (cooldown <= 0.65F && cooldown > 0.35F) {
+                    args.set(4, OverlayTweaksConfig.CONFIG.instance().shieldColorMid.getRed() / 255F);
+                    args.set(5, OverlayTweaksConfig.CONFIG.instance().shieldColorMid.getGreen() / 255F);
+                    args.set(6, OverlayTweaksConfig.CONFIG.instance().shieldColorMid.getBlue() / 255F);
+                } else if (cooldown <= 0.35F && cooldown > 0F) {
+                    args.set(4, OverlayTweaksConfig.CONFIG.instance().shieldColorLow.getRed() / 255F);
+                    args.set(5, OverlayTweaksConfig.CONFIG.instance().shieldColorLow.getGreen() / 255F);
+                    args.set(6, OverlayTweaksConfig.CONFIG.instance().shieldColorLow.getBlue() / 255F);
+                }
             }
         }
     }
